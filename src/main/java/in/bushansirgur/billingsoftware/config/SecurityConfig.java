383fc80662f0033ae543abd9965e148ad7f55f27
Saikaranam-70 <saikaranam46@gmail.com>
2025-10-12 10:53:18 +0530

    Updated API routes and fixed bugs

--- src/main/java/in/bushansirgur/billingsoftware/config/SecurityConfig.java
+++ src/main/java/in/bushansirgur/billingsoftware/config/SecurityConfig.java
@@ -59,7 +59,7 @@ public class SecurityConfig {
 
     private UrlBasedCorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("https://billing-software-omega.vercel.app/"));
+        config.setAllowedOrigins(List.of("https://billing-software-omega.vercel.app/", "http://localhost:5173/"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         config.setAllowCredentials(true);
